a5b0c3ce703308891506e3611256b6c50be5b301
Thomas <thomasjefferson.dev@gmail.com>
2025-07-19 22:15:54 -0300

    feat: Exemple of operators and relational expressions added

---
+++ lib/conditionals/operators_expressions/operators_expressions.dart
@@ -0,0 +1,36 @@
+void main() {
+  /// Operators:
+  /// > greatter than
+  /// >= greatter than or equal to
+  /// < less than
+  /// <= less than or equal to
+  /// == equal to
+  /// != different from
+
+  /// Relational expressions:
+  /// 2 > 1 result: true
+  /// 1 > 2 result: false
+  /// 2 >= 1 result: true
+  /// 1 >= 2 result: false
+  /// 1 < 2 result: true
+  /// 2 < 1 result: false
+  /// 1 <= 2 result: true
+  /// 2 <= 1 result: false
+  /// 1 == 1 result: true
+  /// 1 == 2 result: false
+  /// 1 != 2 result: true
+  /// 1 != 1 result: false
+
+  print(2 > 1);
+  print(1 > 2);
+  print(2 >= 1);
+  print(1 >= 2);
+  print(1 < 2);
+  print(2 < 1);
+  print(1 <= 2);
+  print(2 <= 1);
+  print(1 == 1);
+  print(1 == 2);
+  print(1 != 2);
+  print(1 != 1);
+}
